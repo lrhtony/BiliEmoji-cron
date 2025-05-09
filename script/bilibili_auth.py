eccaf779c476b93a668b3c681528d911f35e5e4c
lrhtony <tony07017@163.com>
2025-05-10 03:35:07 +0800

    feat: add Bilibili authentication and emoji management functionality

---
+++ script/bilibili_auth.py
@@ -0,0 +1,143 @@
+# -*- coding: UTF-8 -*-
+import os
+import time
+import urllib.parse
+import hashlib
+from datetime import datetime
+import pymongo
+import requests
+import certifi
+
+
+def appsign(params, appkey, appsec):
+    """
+    对请求参数进行签名
+    :param params: 请求参数字典
+    :param appkey: 应用的 appkey
+    :param appsec: 应用的 appsec
+    :return: 带签名的请求参数字典
+    """
+    params.update({'appkey': appkey})
+    params = dict(sorted(params.items()))  # 按 key 排序参数
+    query = urllib.parse.urlencode(params)  # 序列化参数
+    sign = hashlib.md5((query + appsec).encode()).hexdigest()  # 计算签名
+    params.update({'sign': sign})
+    return params
+
+
+def concat_cookies(cookies):
+    """
+    将 cookie 列表拼接成字符串
+    :param cookies: cookie 列表，每个元素是字典，包含 name 和 value
+    :return: 拼接后的 cookie 字符串
+    """
+    return ';'.join(f"{cookie['name']}={cookie['value']}" for cookie in cookies)
+
+
+class BilibiliAuth:
+    """
+    Bilibili 认证类，用于管理账号的 access_token 和 cookie
+    """
+    def __init__(self, db_uri=None):
+        """
+        初始化 BilibiliAuth 实例
+        :param db_uri: 数据库连接 URI，默认为环境变量 ACCOUNT_DB_URI
+        """
+        self.MONGO_CLIENT = pymongo.MongoClient(
+            db_uri or os.getenv('ACCOUNT_DB_URI', 'mongodb://localhost:27017/'),
+            tlsCAFile=certifi.where()
+        )
+
+    def get_access(self, mid):
+        """
+        获取指定用户的 access_token 和 cookie
+        :param mid: 用户的 mid
+        :return: access_token 和 cookie 字符串
+        """
+        account_info = self.MONGO_CLIENT['bilibili']['accounts'].find_one({'mid': mid})
+        access_key = account_info['token_info']['access_token']
+        refresh_token = account_info['token_info']['refresh_token']
+        cookie_info = account_info['cookie_info']['cookies']
+        cookie_str = concat_cookies(cookie_info)
+        last_update = account_info['last_update']
+
+        # 如果距离上次更新时间超过 30 天，则重新获取 access_token
+        if (datetime.now() - last_update).days > 30:
+            access_key, cookie_str = self.refresh_access_token(access_key, refresh_token)
+        return access_key, cookie_str
+
+    def refresh_access_token(self, access_key, refresh_token):
+        """
+        刷新 access_token 并更新数据库
+        :param access_key: 当前的 access_token
+        :param refresh_token: 用于刷新 access_token 的 refresh_token
+        :return: 新的 access_token 和 cookie 字符串
+        """
+        # 请求头
+        headers = {
+            'env': 'prod',
+            'app-key': 'android_i',
+            'user-agent': 'Mozilla/5.0 BiliDroid/3.20.4 (bbcallen@gmail.com) os/android model/Mi 10 mobi_app/android_i build/8230800 channel/master innerVer/8230800 osVer/13 network/2',
+            'bili-http-engine': 'cronet',
+            'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+            'accept-encoding': 'gzip, deflate, br'
+        }
+
+        # 请求数据
+        data = {
+            'access_key': access_key,
+            'build': '6750200',
+            'c_locale': 'zh_CN',
+            'channel': 'master',
+            'device': 'phone',
+            'disable_rcmd': '0',
+            'from_access_key': access_key,
+            'mobi_app': 'android_i',
+            'platform': 'android',
+            'refresh_token': refresh_token,
+            's_locale': 'zh_CN',
+            'statistics': '{"appId":14,"platform":3,"version":"3.20.4","abtest":""}',
+            'sts': int(time.time()),
+            'ts': int(time.time())
+        }
+
+        # 对数据进行签名
+        data_sign = appsign(data, 'ae57252b0c09105d', 'c75875c596a69eb55bd119e74b07cfe3')
+
+        # 发送请求刷新 token
+        response = requests.post(
+            'https://passport.bilibili.com/x/passport-login/oauth2/refresh_token',
+            headers=headers,
+            data=data_sign
+        )
+
+        # 记录日志
+        self.MONGO_CLIENT['bilibili']['logs'].insert_one({'date': datetime.now(), 'response': response.text})
+
+        # 解析响应
+        res = response.json()
+        if res['code'] == 0:
+            # 更新数据库中的 token 和 cookie 信息
+            self.MONGO_CLIENT['bilibili']['accounts'].update_one(
+                {'mid': res['data']['token_info']['mid']},
+                {
+                    '$set': {
+                        'token_info': res['data']['token_info'],
+                        'cookie_info': res['data']['cookie_info'],
+                        'last_update': datetime.now()
+                    }
+                }
+            )
+            access_key = res['data']['token_info']['access_token']
+            cookie_info = res['data']['cookie_info']['cookies']
+            cookie_str = concat_cookies(cookie_info)
+            return access_key, cookie_str
+        else:
+            raise Exception('Refresh token failed!')
+
+
+if __name__ == '__main__':
+    # 示例用法
+    auth = BilibiliAuth()
+    access_token, cookie_str = auth.get_access(mid=1)
+    # print(f"Access Token: {access_token}")
--- script/main.py
+++ script/main.py
@@ -1,31 +1,31 @@
 # -*- coding: UTF-8 -*-
 import requests
 import json
+import time
 import os
+
+from bilibili_auth import BilibiliAuth, appsign
 
 
 class BiliEmoji:
+    """
+    用于获取B站表情包信息并保存的工具类
+    """
     def __init__(self):
-        self.PROXY = json.loads(os.getenv('PROXY', '{}'))
-        self.SCAN_CONFIG = json.loads(os.getenv('SCAN_CONFIG', '[]'))
-        # self.PROXY = {'http': 'http://127.0.0.1:6667', 'https': 'http://127.0.0.1:6667'}
-        # self.SCAN_CONFIG = [1, 550, 20, [4, 250]]  # [初始值, 结束值, 步长, 忽略值]
+        # 初始化配置和认证信息
+        self.PROXY = json.loads(os.getenv('PROXY', '{}'))  # 代理配置
+        self.SCAN_CONFIG = json.loads(os.getenv('SCAN_CONFIG', '{"start": 1, "end": 10000, "step": 40, "ignore": [4, 250]}'))  # 扫描配置
 
-    def main(self):
-        ids = list(range(self.SCAN_CONFIG[0], self.SCAN_CONFIG[1]))
-        ignore_ids = self.SCAN_CONFIG[3]
-        for ignore_id in ignore_ids:  # 删除一些不必要的id
-            try:
-                ids.remove(ignore_id)
-            except ValueError:
-                pass
-        for i in range(0, len(ids), self.SCAN_CONFIG[2]):
-            id_list = ids[i:i + self.SCAN_CONFIG[2]]
-            emojis_info = self.get_emoji_info(id_list)  # 批量获取表情包信息
-            for emoji_info in emojis_info:
-                self.save_emoji_info(emoji_info)  # 逐个保存表情包信息
+        self.ACCOUNT = int(os.getenv('ACCOUNT', 1))  # 账号ID
+        self.AUTH = BilibiliAuth(os.getenv('ACCOUNT_DB_URI', 'mongodb://localhost:27017/'))  # 认证模块
+        self.ACCESS_KEY, self.COOKIE = self.AUTH.get_access(self.ACCOUNT)  # 获取访问密钥和Cookie
 
     def get_emoji_info(self, ids: list) -> list:
+        """
+        根据表情包ID列表获取表情包信息
+        :param ids: 表情包ID列表
+        :return: 表情包信息列表
+        """
         params = {
             'business': 'reply',
             'ids': ','.join([str(i) for i in ids]),
@@ -39,54 +39,118 @@ class BiliEmoji:
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh-CN,zh;q=0.9'
         }
+        # 发送请求获取表情包信息
         response = requests.get('https://api.bilibili.com/x/emote/package', params=params, proxies=self.PROXY, headers=headers)
         response_json = response.json()
-        if response_json['code'] != 0:  # 乱七八糟的情况
+        if response_json['code'] != 0:  # 检查返回结果是否正常
             raise Exception(response_json['message'])
 
         result = []
         packages = response_json['data']['packages']
         if packages:
             for package in packages:
-                package_id = package['id']
-                package_text = package['text']
-                package_icon = package['url'].replace('http://', 'https://')
-                package_resource_type = package['resource_type']
-                package_dict = {
-                    'id': package_id,
-                    'text': package_text,
-                    'icon': package_icon,
-                    'resource_type': package_resource_type,
-                }
-                if 'emote' in package:
-                    emote_list = []
-                    emotes = package['emote']
-                    for emote in emotes:
-                        emote_text = emote['text'].replace('[', '').replace(']', '')
-                        emote_url = emote['url'].replace('http://', 'https://')
-                        emote_data = {
-                            'text': emote_text,
-                            'url': emote_url,
-                        }
-                        if 'gif_url' in emote:  # 新出的动态表情
-                            emote_data['gif_url'] = emote['gif_url'].replace('http://', 'https://')
-                        if 'webp_url' in emote:
-                            emote_data['webp_url'] = emote['webp_url'].replace('http://', 'https://')
-                        emote_list.append(emote_data)
-                    package_dict['emote'] = emote_list
+                package_dict = self._parse_package(package)  # 解析表情包信息
                 result.append(package_dict)
         return result
 
     @staticmethod
+    def _parse_package(package: dict) -> dict:
+        """
+        解析单个表情包信息
+        :param package: 表情包数据
+        :return: 解析后的表情包字典
+        """
+        package_dict = {
+            'id': package['id'],
+            'text': package['text'],
+            'icon': package['url'].replace('http://', 'https://'),
+            'resource_type': package['resource_type'],
+        }
+        if 'emote' in package:
+            emote_list = []
+            for emote in package['emote']:
+                emote_data = {
+                    'text': emote['text'].replace('[', '').replace(']', ''),
+                    'url': emote['url'].replace('http://', 'https://'),
+                }
+                if 'gif_url' in emote:  # 动态表情
+                    emote_data['gif_url'] = emote['gif_url'].replace('http://', 'https://')
+                if 'webp_url' in emote:
+                    emote_data['webp_url'] = emote['webp_url'].replace('http://', 'https://')
+                emote_list.append(emote_data)
+            package_dict['emote'] = emote_list
+        return package_dict
+
+    @staticmethod
     def save_emoji_info(emoji_info: dict):
+        """
+        保存表情包信息到本地文件
+        :param emoji_info: 表情包信息字典
+        """
         emoji_id = emoji_info['id']
         emoji_text = emoji_info['text']
         filename = f'{emoji_id}-{emoji_text}.json'
         filepath = os.path.join('list', filename)
+        os.makedirs('list', exist_ok=True)  # 确保目录存在
         with open(filepath, 'w', encoding='utf-8') as f:
             f.write(json.dumps(emoji_info, ensure_ascii=False, indent=2))
+
+    def get_latest_emoji_id(self) -> int:
+        """
+        获取最新的表情包ID
+        :return: 最新表情包ID
+        """
+        params = {
+            'access_key': self.ACCESS_KEY,
+            'build': 8230800,
+            'business': 'reply',
+            'channel': 'master',
+            'disable_rcmd': 0,
+            'mobi_app': 'android_i',
+            'platform': 'android',
+            'pn': 1,
+            'ps': 100,
+            'search': '',
+            'statistics': '{"appId":14,"platform":3,"version":"3.20.4","abtest":""}',
+            'ts': int(time.time())
+        }
+        headers = {
+            'native_api_from': 'h5',
+            'cookie': self.COOKIE,
+            'accept': 'application/json, text/plain, */*',
+            'referer': 'https://www.bilibili.com/h5/mall/emoji-package/more?navhide=1&native.theme=0',
+            'content-type': 'application/json',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 13; Mi 10 Build/TKQ1.221114.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/135.0.7049.111 Mobile Safari/537.36 os/android model/Mi 10 build/8230800 osVer/13 sdkInt/33 network/2 BiliApp/8230800 mobi_app/android_i channel/master innerVer/8230800 c_locale/zh_CN s_locale/zh_CN disable_rcmd/0 themeId/0 sh/33 3.20.4 os/android model/Mi 10 mobi_app/android_i build/8230800 channel/master innerVer/8230800 osVer/13 network/2',
+            'bili-http-engine': 'cronet',
+            'accept-encoding': 'gzip, deflate, br'
+        }
+        sign_params = appsign(params, 'bb3101000e232e27', '36efcfed79309338ced0380abd824ac1')
+        response = requests.get(
+            'https://api.bilibili.com/bapis/main.community.interface.emote.EmoteService/AllPackages',
+            params=sign_params, proxies=self.PROXY, headers=headers)
+        res = response.json()
+        if res['code'] != 0:
+            # 警告并返回SCAN_CONFIG['end']
+            print(f"[WARN] 获取最新表情包id失败: {res['message']}")
+            return self.SCAN_CONFIG['end']
+        # 遍历所有表情包，获取最新的表情包id
+        return max(package['id'] for package in res['data']['packages'])
+
+    def main(self):
+        """
+        主函数，获取表情包信息并保存
+        """
+        end_id = self.get_latest_emoji_id()
+        ids = list(range(self.SCAN_CONFIG['start'], end_id + 50))  # 获取表情包id列表
+        ignore_ids = self.SCAN_CONFIG['ignore']  # 需要忽略的id
+        ids = [id_ for id_ in ids if id_ not in ignore_ids]  # 删除不必要的id
+        for i in range(0, len(ids), self.SCAN_CONFIG['step']):  # 分批获取表情包信息
+            id_list = ids[i:i + self.SCAN_CONFIG['step']]
+            emojis_info = self.get_emoji_info(id_list)  # 批量获取表情包信息
+            for emoji_info in emojis_info:
+                self.save_emoji_info(emoji_info)  # 逐个保存表情包信息
 
 
 if __name__ == "__main__":
     BiliEmoji = BiliEmoji()
-    BiliEmoji.main()
+    BiliEmoji.main()  # 主函数调用
